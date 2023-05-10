25c56659ffd58014fefa0f9fd5e51ac7f4210a39
Eben2023 <ebenprec@yahoo.com>
2023-05-09 21:00:31 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for letter in range(90, 64, -1):
-    print("{:c}".format(letter + 32 * (letter % 2)), end="")
-print()
+for letter in range(ord('z'), ord('a') - 1, -1):
+    if letter % 2 != 0:
+        letter = chr(letter - ord('a') + ord('A'))
+    else:
+        letter = chr(letter)
+    print("{}".format(letter), end="")
