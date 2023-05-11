3e2a14d17aa1988a999d03353c26318306aac010
Eben2023 <ebenprec@yahoo.com>
2023-05-11 16:47:47 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-import importlib.util
-def get_names(module_name):
-    spec = importlib.util.spec_from_file_location(module_name, module_name + ".pyc")
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return [name for name in dir(module) if not name.startswith("__")]
+#!/usr/bin/python3.8
+import hidden_4
+
 if __name__ == "__main__":
-    names = get_names("hidden_4")
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
     names.sort()
     for name in names:
         print(name)
