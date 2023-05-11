42c1605d260390fadebfd1ac5aaaa60885d0ee93
Eben2023 <ebenprec@yahoo.com>
2023-05-11 16:13:10 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = sys.argv[1:]  # Ignore the first argument (the script name)
-result = sum(int(arg) for arg in args)
-print(result)
+    args = sys.argv[1:]  # Ignore the first argument (the script name)
+    result = sum(int(arg) for arg in args)
+    print(result)
