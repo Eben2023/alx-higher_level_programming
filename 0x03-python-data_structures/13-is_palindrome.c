fea9c13ccce5cdfa1d081b6dfaa140df0138dc4d
Eben2023 <ebenprec@yahoo.com>
2023-05-15 03:12:50 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,92 +1,100 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to head of list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ * If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow_ptr = *head, *fast_ptr = *head;
-    listint_t *prev_slow_ptr = NULL, *mid_ptr = NULL;
-    listint_t *second_half = NULL;
-    int is_palindrome = 1;
+listint_t *slow_ptr, *fast_ptr, *prev_slow_ptr, *mid_node, *second_half;
+listint_t *tmp = *head;
+int is_pal = 1;
 
-    if (*head != NULL && (*head)->next != NULL)
-    {
-        while (fast_ptr != NULL && fast_ptr->next != NULL)
-        {
-            fast_ptr = fast_ptr->next->next;
-            prev_slow_ptr = slow_ptr;
-            slow_ptr = slow_ptr->next;
-        }
-
-        if (fast_ptr != NULL)
-        {
-            mid_ptr = slow_ptr;
-            slow_ptr = slow_ptr->next;
-        }
-
-        second_half = slow_ptr;
-        prev_slow_ptr->next = NULL;
-        reverse_list(&second_half);
-        is_palindrome = compare_lists(*head, second_half);
-        reverse_list(&second_half);
-
-        if (mid_ptr != NULL)
-        {
-            prev_slow_ptr->next = mid_ptr;
-            mid_ptr->next = second_half;
-        }
-        else
-            prev_slow_ptr->next = second_half;
-    }
-
-    return is_palindrome;
-}
-
-/**
- * reverse_list - reverses a singly linked list
- * @head: pointer to head of list
- */
-void reverse_list(listint_t **head)
+if (*head != NULL && (*head)->next != NULL)
 {
-    listint_t *prev_node = NULL, *current_node = *head, *next_node = NULL;
+slow_ptr = *head;
+fast_ptr = *head;
+prev_slow_ptr = *head;
 
-    while (current_node != NULL)
-    {
-        next_node = current_node->next;
-        current_node->next = prev_node;
-        prev_node = current_node;
-        current_node = next_node;
-    }
+while (fast_ptr != NULL && fast_ptr->next != NULL)
+{
+fast_ptr = fast_ptr->next->next;
+prev_slow_ptr = slow_ptr;
+slow_ptr = slow_ptr->next;
+}
 
-    *head = prev_node;
+if (fast_ptr != NULL)
+{
+mid_node = slow_ptr;
+slow_ptr = slow_ptr->next;
+}
+
+second_half = slow_ptr;
+prev_slow_ptr->next = NULL;
+reverse_listint(&second_half);
+is_pal = compare_lists(*head, second_half);
+reverse_listint(&second_half);
+
+if (mid_node != NULL)
+{
+prev_slow_ptr->next = mid_node;
+mid_node->next = second_half;
+}
+else
+{
+prev_slow_ptr->next = second_half;
+}
+}
+
+return is_pal;
 }
 
 /**
- * compare_lists - compares two singly linked lists
- * @head1: pointer to head of first list
- * @head2: pointer to head of second list
- * Return: 0 if the lists are not equal, 1 if they are equal
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *prev = NULL;
+listint_t *current = *head;
+listint_t *next = NULL;
+
+while (current != NULL)
+{
+next = current->next;
+current->next = prev;
+prev = current;
+current = next;
+}
+
+*head = prev;
+return *head;
+}
+
+/**
+ * compare_lists - Compares two singly linked lists for equality.
+ * @head1: A pointer to the head of the first linked list.
+ * @head2: A pointer to the head of the second linked list.
+ *
+ * Return: If the two lists are equal - 1.
+ * Otherwise - 0.
  */
 int compare_lists(listint_t *head1, listint_t *head2)
 {
-    listint_t *temp1 = head1, *temp2 = head2;
+while (head1 != NULL && head2 != NULL)
+{
+if (head1->n != head2->n)
+{
+return 0;
+}
+head1 = head1->next;
+head2 = head2->next;
+}
 
-    while (temp1 && temp2)
-    {
-        if (temp1->n == temp2->n)
-        {
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        else
-            return 0;
-    }
-
-    if (temp1 == NULL && temp2 == NULL)
-        return 1;
-
-    return 0;
+return (head1 == NULL && head2 == NULL);
 }
