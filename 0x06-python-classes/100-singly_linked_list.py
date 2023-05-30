4a94b75ea6ce2b734f025b797c3f3053af2dfef9
Eben2023 <ebenprec@yahoo.com>
2023-05-30 14:31:48 -0700

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -75,7 +75,8 @@ class SinglyLinkedList:
             return
 
         current = self.head
-        while current.next_node is not None and current.next_node.data< value:
+        while (current.next_node is not None and
+        current.next_node.data < value):
             current = current.next_node
 
         new_node.next_node = current.next_node
