98ad3682860f84e9b602a5c8ea25a33a36f60c93
Eben2023 <ebenprec@yahoo.com>
2023-05-30 12:54:09 -0700

    update

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Represents a square.
+
+    This class doesn't have any attributes or methods defined.
+    It serves as a blueprint for creating square objects.
+    """
+    pass
