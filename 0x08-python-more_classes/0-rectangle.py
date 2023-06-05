da412d9b9ab7b3e7bd09ef22ecedff0c753ec803
Eben2023 <ebenprec@yahoo.com>
2023-06-05 16:12:55 -0700

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Module: 0-rectangle
+This module defines the Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty rectangle.
+        """
+        pass
