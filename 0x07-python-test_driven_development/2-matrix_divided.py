91627a04714573e5b13967522725bf6751bdba15
Eben2023 <ebenprec@yahoo.com>
2023-06-07 11:05:30 -0700

    some failed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -24,23 +24,26 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If `div` is equal to 0.
 
     """
+    # Validate matrix input
     if not isinstance(matrix, list) or not \
             all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) \
-                        of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    # Validate matrix rows size
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
+    # Validate div input
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
+    # Validate div is not zero
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = []
-    for row in matrix:
-        new_row = [round(element / div, 2) for element in row]
-        new_matrix.append(new_row)
+    # Perform division and round to 2 decimal places
+    new_matrix = [[round(elem / div, 2) for elem in row] for row in matrix]
 
     return new_matrix
