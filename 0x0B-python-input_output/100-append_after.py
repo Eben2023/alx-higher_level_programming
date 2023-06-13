c2f981be965fd61b8bb517e1f1144b87e4c58f65
Eben2023 <ebenprec@yahoo.com>
2023-06-13 15:14:46 -0700

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,32 +4,25 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Insert a line of text after each line containing a specific string.
+    Inserts a line of text after each line containing a specific
+    string in a file.
 
     Args:
-        filename (str): The name of the file to modify.
-        search_string (str): The string to search for in each line.
-        new_string (str): The line of text to insert after each matching
-        line.
+        filename (str): The name of the file.
+        search_string (str): The specific string to search for
+        in each line.
+        new_string (str): The line of text to be inserted after
+        the lines containing the search string.
 
     Returns:
         None
-
-    Raises:
-        FileNotFoundError: If the specified file does not exist.
     """
 
-    try:
-        with open(filename, 'r+') as file:
-            lines = file.readlines()
-            file.seek(0)
-
-            for line in lines:
-                file.write(line)
-                if search_string in line:
-                    file.write(new_string)
-
-            file.truncate()
-
-    except FileNotFoundError:
-        raise FileNotFoundError(f"File '{filename}' does not exist.")
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
