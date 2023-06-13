1229e8a8a0b3d871a0f0cbeba719c92e6fa49c5c
Eben2023 <ebenprec@yahoo.com>
2023-06-13 15:18:32 -0700

    update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,54 @@
 #!/usr/bin/python3
 """Module for print_metrics"""
+
+
 import sys
 
-
-def print_metrics(status_codes, total_size):
+def print_metrics(file_sizes, status_codes):
     """
-    Print the computed metrics based on the provided statistics.
+    Prints the computed metrics.
 
     Args:
-        status_codes (dict): A dictionary containing the status codes and their counts.
-        total_size (int): The total file size.
+        file_sizes (list): List of file sizes.
+        status_codes (dict): Dictionary of status codes and their counts.
 
     Returns:
         None
     """
 
-    print("File size: {}".format(total_size))
+    total_size = sum(file_sizes)
+    print(f"File size: {total_size}")
 
     for status_code in sorted(status_codes):
         count = status_codes[status_code]
-        print("{}: {}".format(status_code, count))
+        print(f"{status_code}: {count}")
+
+def compute_metrics():
+    """
+    Reads stdin line by line and computes metrics.
+
+    Returns:
+        None
+    """
+
+    file_sizes = []
+    status_codes = defaultdict(int)
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+
+            parts = line.strip().split()
+            file_size = int(parts[-1])
+            status_code = parts[-2]
+
+            file_sizes.append(file_size)
+            status_codes[status_code] += 1
+
+            if line_count % 10 == 0:
+                print_metrics(file_sizes, status_codes)
+
+    except KeyboardInterrupt:
+        print_metrics(file_sizes, status_codes)
+        raise
